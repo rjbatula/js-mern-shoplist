8b60fcb7f35e3e7fed08c0ed77e5dbf95c56f3c9
rjbatula <rjbatula@yahoo.com>
2020-06-06 00:15:01 +0800

    Add files via upload
    --

---
+++ precache-manifest.e3092cb78ce0d0223fda751810f11363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95eb8b43d9e114028ebf791e1409941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2136973343cc59c20f59",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d653637668efef6a7b0d",
+    "url": "/static/css/main.dfe5296b.chunk.css"
+  },
+  {
+    "revision": "2136973343cc59c20f59",
+    "url": "/static/js/2.4774bfd6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4774bfd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d653637668efef6a7b0d",
+    "url": "/static/js/main.46fad749.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
